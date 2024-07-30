0bee324bb0436c683d0791ab75d505f07f9fdf38
Oleg Sinavski <2086260+olegsinavski@users.noreply.github.com>
2024-07-30 15:27:16 +0100

    older torch

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open("README.md", "r") as f:
 
 # Required dependencies
 REQUIRED_PACKAGES = [
-    "torch>=2.3.1",
+    "torch>=2.1.2",
     "torchvision>=0.18.1",
     "numpy>=1.24.4",
     "tqdm>=4.66.1",

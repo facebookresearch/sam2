953e09e27b16b1ed3bef8679c5c65f2f69f04826
Adam Jochna <adam@encord.com>
2024-07-30 18:36:06 +0200

    Disable ext_modules

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 
 from setuptools import find_packages, setup
-from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+from torch.utils.cpp_extension import BuildExtension
 
 # Package metadata
 NAME = "SAM 2"
@@ -38,18 +38,7 @@ EXTRA_PACKAGES = {
 
 
 def get_extensions():
-    srcs = ["sam2/csrc/connected_components.cu"]
-    compile_args = {
-        "cxx": [],
-        "nvcc": [
-            "-DCUDA_HAS_FP16=1",
-            "-D__CUDA_NO_HALF_OPERATORS__",
-            "-D__CUDA_NO_HALF_CONVERSIONS__",
-            "-D__CUDA_NO_HALF2_OPERATORS__",
-        ],
-    }
-    ext_modules = [CUDAExtension("sam2._C", srcs, extra_compile_args=compile_args)]
-    return ext_modules
+    return []
 
 
 # Setup configuration

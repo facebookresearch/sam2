8fecf896e069e7a330c98ec6470082c147ac864a
Jean-Christophe Ruel <ruelj2@gmail.com>
2024-07-30 19:11:34 -0400

    Change the hydra initialization method to accept relative path for config files

--- sam2/__init__.py
+++ sam2/__init__.py
@@ -4,6 +4,6 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-from hydra import initialize_config_module
+from hydra import initialize
 
-initialize_config_module("sam2_configs", version_base="1.2")
+initialize("sam2_configs", version_base="1.2")

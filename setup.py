c8036ab075b7345261e38f2678a5d14fff759bcb
Christian Puhrsch <cpuhrsch@gmail.com>
2024-09-21 19:10:24 +0000

    Disable dependencies

--- setup.py
+++ setup.py
@@ -22,13 +22,13 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 # Required dependencies
 REQUIRED_PACKAGES = [
-    "torch>=2.3.1",
-    "torchvision>=0.18.1",
-    "numpy>=1.24.4",
-    "tqdm>=4.66.1",
-    "hydra-core>=1.3.2",
-    "iopath>=0.1.10",
-    "pillow>=9.4.0",
+    # "torch>=2.3.1",
+    # "torchvision>=0.18.1",
+    # "numpy>=1.24.4",
+    # "tqdm>=4.66.1",
+    # "hydra-core>=1.3.2",
+    # "iopath>=0.1.10",
+    # "pillow>=9.4.0",
 ]
 
 EXTRA_PACKAGES = {
@@ -139,8 +139,8 @@ setup(
     packages=find_packages(exclude="notebooks"),
     package_data={"": ["*.yaml"]},  # SAM 2 configuration files
     include_package_data=True,
-    install_requires=REQUIRED_PACKAGES,
-    extras_require=EXTRA_PACKAGES,
+    # install_requires=REQUIRED_PACKAGES,
+    # extras_require=EXTRA_PACKAGES,
     python_requires=">=3.10.0",
     ext_modules=get_extensions(),
     cmdclass=cmdclass,

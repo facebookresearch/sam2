363a66fb2e913a0183bd5211fa14d976a7cfcd58
Thibaut Chataing <Thibaut.Chataing@unige.ch>
2024-10-29 13:23:55 +0100

    change setup.py with our own information for the package

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ import os
 from setuptools import find_packages, setup
 
 # Package metadata
-NAME = "SAM-2"
+NAME = "SAM-2-LV"
 VERSION = "1.0"
-DESCRIPTION = "SAM 2: Segment Anything in Images and Videos"
-URL = "https://github.com/facebookresearch/sam2"
-AUTHOR = "Meta AI"
-AUTHOR_EMAIL = "segment-anything@meta.com"
+DESCRIPTION = "SAM 2 Longue Video: Segment Anything for longue Videos"
+URL = "https://github.com/ChataingT/sam2_longer_video"
+AUTHOR = "Mirko, Nicolò, Thibaut"
+AUTHOR_EMAIL = "thibaut.chataing@unige.ch"
 LICENSE = "Apache 2.0"
 
 # Read the contents of README file
@@ -37,17 +37,6 @@ EXTRA_PACKAGES = {
         "jupyter>=1.0.0",
         "opencv-python>=4.7.0",
         "eva-decord>=0.6.1",
-    ],
-    "interactive-demo": [
-        "Flask>=3.0.3",
-        "Flask-Cors>=5.0.0",
-        "av>=13.0.0",
-        "dataclasses-json>=0.6.7",
-        "eva-decord>=0.6.1",
-        "gunicorn>=23.0.0",
-        "imagesize>=1.4.1",
-        "pycocotools>=2.0.8",
-        "strawberry-graphql>=0.243.0",
     ],
     "dev": [
         "black==24.2.0",

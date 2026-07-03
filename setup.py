a6c5f84de1fca0d472efcabd47900b0b43bfd41e
陈家名 <chenjiaming@kezaihui.com>
2026-07-03 11:06:58 +0800

    Fix package exclude patterns

--- setup.py
+++ setup.py
@@ -164,7 +164,7 @@ setup(
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
     license=LICENSE,
-    packages=find_packages(exclude="notebooks"),
+    packages=find_packages(exclude=["notebooks", "notebooks.*"]),
     include_package_data=True,
     install_requires=REQUIRED_PACKAGES,
     extras_require=EXTRA_PACKAGES,
